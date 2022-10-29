289b84f6b06c20ba675f18420d80dfdb972dfb4e
donghyeon.seo <82585734+sdh20282@users.noreply.github.com>
2022-10-30 00:54:13 +0900

    Update 징검다리.py

--- Programmers/Level4/징검다리.py
+++ Programmers/Level4/징검다리.py
@@ -1,1 +1,30 @@
+def solution(distance, rocks, n):
+    rocks.sort()
+    distances = []
 
+    prev = 0
+    for rock in rocks:
+        distances.append(rock - prev)
+        prev = rock
+    distances.append(distance - prev)
+
+    start, end = 0, distance
+
+    while end - start > 1:
+        mid = (start + end) // 2
+        now_distances = distances.copy()
+        current = removed = 0
+
+        for now_distance in now_distances:
+            current += now_distance
+            if current < mid:
+                removed += 1
+            else:
+                current = 0
+
+        if removed <= n:
+            start = mid
+        else:
+            end = mid - 1
+
+    return end
