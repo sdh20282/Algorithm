52cf6f11e2bb820ea4000870ea21869ee65ea64f
donghyeon.seo <82585734+sdh20282@users.noreply.github.com>
2023-02-06 14:39:55 +0900

    Create 2 x n 타일링.js

---
+++ Programmers/Level2/2 x n 타일링.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    const pibo = Array(n).fill(0)
+    pibo[0] = 1;
+    pibo[1] = 2;
+    
+    for(let i = 2; i < n; i++){
+        if ((pibo[i-1] + pibo[i-2]) > 1000000007) {
+            pibo[i] = (pibo[i-1] + pibo[i-2]) % 1000000007
+        } else {
+            pibo[i] = (pibo[i-1] + pibo[i-2])
+        }
+    }
+    
+    return pibo[n-1];
+}
