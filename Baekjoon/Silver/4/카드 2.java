6bb44c9a1108e728ee28d47a06b8c8d46f124357
donghyeon.seo <82585734+sdh20282@users.noreply.github.com>
2023-02-10 17:02:52 +0900

    Docs : 주석 수정

--- Baekjoon/Silver/4/카드 2.java
+++ Baekjoon/Silver/4/카드 2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main {
 	public static void main(String args[]) {
-		// 규칙 : (2 * n) - (n보다 같거나 큰 2의 배수)
+		// 규칙 : (2 * n) - (n보다 같거나 큰 2의 제곱수)
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(), k = 1;
 		
