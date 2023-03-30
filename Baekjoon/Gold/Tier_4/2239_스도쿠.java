b676b08ffceb835c1518e76a9bfcb040f939e20f
donghyeon.seo <82585734+sdh20282@users.noreply.github.com>
2023-03-30 17:49:23 +0900

    Docs : 2239_스도쿠 사용하지 않는 변수 삭제

--- Baekjoon/Gold/Tier_4/2239_스도쿠.java
+++ Baekjoon/Gold/Tier_4/2239_스도쿠.java
@@ -1,16 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 	static int[][] map;
-	static boolean end;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		map = new int[9][9];
-		end = false;
 
 		for (int i = 0; i < 9; i++) {
 			String now = br.readLine();
