bc23e75f3436bdba665f3734a4e625b0600d962b
donghyeon.seo <82585734+sdh20282@users.noreply.github.com>
2023-04-06 11:15:48 +0900

    Docs : 잘못 기재한 코드 수정

--- Baekjoon/Gold/Tier_4/1261_알고스팟.java
+++ Baekjoon/Gold/Tier_4/1261_알고스팟.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,66 +11,44 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken()), D = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
-		int[] dishes = new int[N], kinds = new int[D + 1];
+		int M = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N][M];
+		int[][] min = new int[N][M];
+		int[] dr = {-1, 1, 0, 0};
+		int[] dc = {0, 0, -1, 1};
 		
 		for (int i = 0; i < N; i++) {
-			dishes[i] = Integer.parseInt(br.readLine());
-		}
-		
-		kinds[C] += 1;
-		int count = 1;
-		
-		for (int i = 0; i < K; i++) {
-			if (kinds[dishes[i]] == 0) {
-				count += 1;
-			}
+			String now = br.readLine();
 			
-			kinds[dishes[i]] += 1;
-		}
-		
-		int maxKinds = count;
-		
-		for (int i = 0; i < N - K; i++) {
-			kinds[dishes[i]] -= 1;
-			
-			if (kinds[dishes[i]] == 0) {
-				count -= 1;
-			}
-			
-			kinds[dishes[i + K]] += 1;
-			
-			if (kinds[dishes[i + K]] == 1) {
-				count += 1;
-			}
-			
-			if (count > maxKinds) {
-				maxKinds = count;
+			for (int j = 0; j < M; j++) {
+				map[i][j] = now.charAt(j) - '0';
+				min[i][j] = 10000;
 			}
 		}
 		
-		int index = 0;
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {0, 0, 0});
+		min[0][0] = 0;
 		
-		for (int i = N - K; i < N; i++) {
-			kinds[dishes[i]] -= 1;
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
 			
-			if (kinds[dishes[i]] == 0) {
-				count -= 1;
+			for (int i = 0; i < 4; i++) {
+				int nr = now[0] + dr[i], nc = now[1] + dc[i];
+				
+				if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
+					continue;
+				}
+				
+				if (min[nr][nc] <= now[2] + map[nr][nc]) {
+					continue;
+				}
+				
+				q.offer(new int[] {nr, nc, now[2] + map[nr][nc]});
+				min[nr][nc] = now[2] + map[nr][nc];
 			}
-			
-			kinds[dishes[index]] += 1;
-			
-			if (kinds[dishes[index]] == 1) {
-				count += 1;
-			}
-			
-			if (count > maxKinds) {
-				maxKinds = count;
-			}
-			
-			index += 1;
-		}	
-
-		System.out.println(maxKinds);
+		}
+		
+		System.out.println(min[N - 1][M - 1]);
 	}
 }
