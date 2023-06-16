482ed908ea1c624dac36db0609e57b21f35d7d47
donghyeon.seo <82585734+sdh20282@users.noreply.github.com>
2023-06-16 22:30:21 +0900

    Create 11650_좌표 정렬하기.java

---
+++ Baekjoon/Silver/Tier_5/11650_좌표 정렬하기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[][] arr = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+
+		Arrays.sort(arr, (e1, e2) -> {
+			if (e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+		
+		sc.close();
+	}
+}
