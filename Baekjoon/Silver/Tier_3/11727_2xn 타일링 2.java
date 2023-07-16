9ffa87b5dadc09d4234f1713f85705af4e458232
donghyeon.seo <sdh20282@gmail.com>
2023-07-16 22:12:01 +0900

    Create 11727_2xn 타일링 2.java

---
+++ Baekjoon/Silver/Tier_3/11727_2xn 타일링 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++)
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+
+        System.out.println(dp[n]);
+        sc.close();
+    }
+}
