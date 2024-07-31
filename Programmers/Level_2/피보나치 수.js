caf90d7484725d102eac2d79be1d84a87726f8dd
donghyeon.seo <sdh20282@gmail.com>
2024-08-01 02:46:59 +0900

    Create 피보나치 수.js

---
+++ Programmers/Level_2/피보나치 수.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  const dp = new Array(100001).fill(0);
+
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+  }
+
+  return dp[n];
+}
